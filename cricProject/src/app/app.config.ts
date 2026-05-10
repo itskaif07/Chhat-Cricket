d4f5cc2a5d530583d96851b055a3409457ad03c6
Md Kaif <kaifk8402@gmail.com>
2026-05-10 23:52:51 +0530

    firebase installation

--- cricProject/src/app/app.config.ts
+++ cricProject/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FirebaseApp, initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import {provideAuth, getAuth } from '@angular/fire/auth'
+
+import { environment } from '../environments/environment'
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+      provideFirebaseApp(() =>
+      initializeApp(environment.firebaseConfig)
+    ),
+
+      provideAuth(() => getAuth())
   ]
 };
--- cricProject/src/app/app.ts
+++ cricProject/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User} from '@angular/fire/auth'
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('cricProject');
+
+  user: User | null = null
+
+  constructor(private auth: Auth){
+    onAuthStateChanged(this.auth, (user)=>{
+      this.user = user
+    })
+  }
+
+  logIn(){
+    const provider = new GoogleAuthProvider()
+
+    signInWithPopup(this.auth, provider).then(() =>{
+      console.log('Logged In')
+    })
+    .catch((e) =>{
+      console.log("Error: " + e)
+    })
+  }
+
+  logOut(){
+    signOut(this.auth)
+  }
 }
